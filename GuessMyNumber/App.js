d99769d4ceafe65ffc360d5ff06c3bcad7e780eb
Kalle Hahtela <khahtela@gmail.com>
2024-01-15 10:20:50 +0200

    added a background linear and a background image underneath it also made some game logic further so the user can reset and confirm the number. Also after confirming the game screen will be shown --> that is also in the making.

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -1,11 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGame';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
   return (
     <LinearGradient colors={['#124e78', '#6e0e0a']} style={styles.mainScreen}>
-      <StartGameScreen />
+      <ImageBackground source={require('./assets/images/background.png')} resizeMode='cover' style={styles.mainScreen} imageStyle={styles.backgroundImage}>
+        <SafeAreaView style={styles.mainScreen}>{screen}</SafeAreaView>
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   mainScreen: {
     flex: 1
+  },
+  backgroundImage: {
+    opacity: .2
   }
 });
--- GuessMyNumber/components/PrimaryButtons.js
+++ GuessMyNumber/components/PrimaryButtons.js
@@ -1,18 +1,13 @@
 import { View, Text, Pressable, StyleSheet  } from 'react-native';
 
-function PrimaryButton({children}) {
-    function pressHandler() {
-        console.log('Pressed');
-    }
+function PrimaryButton({ children, onPress }) {
 
     return (
         <View style={styles.buttonOuterContainer}>
-            <Pressable style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer} onPress={pressHandler} android_ripple={{color: '#0c3f64'}}>
+            <Pressable style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer} onPress={onPress} android_ripple={{color: '#0c3f64'}}>
                     <Text style={styles.buttonText}>{children}</Text>
             </Pressable>
         </View>
-
-       
     );
 }
 
